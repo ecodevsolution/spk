68daf028e7db6c117e6689220cb38cf72331eef5
BillyFajri <ecodevsolution@gmail.com>
2017-07-22 01:12:35 +0700

    percobaan

---
+++ backend/views/tbl-jadwal/create.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model backend\models\TblJadwal */
+
+$this->title = 'Create Tbl Jadwal';
+$this->params['breadcrumbs'][] = ['label' => 'Tbl Jadwals', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="tbl-jadwal-create">
+
+    <!-- <h1><?= Html::encode($this->title) ?></h1> -->
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
