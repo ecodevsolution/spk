30172afdd9005442808bf01c372cce353ec22711
ecodevsolution <ecodevsolution@gmail.com>
2017-08-04 22:53:52 +0700

    update frontend

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -4,7 +4,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'Tocuemb6oUgWH3Ts8rgcWWZYzT_RrMAi',
+            'cookieValidationKey' => 'GZAFZfQgCbuMd513qivWeQ3Hvhdqt2ZE',
         ],
     ],
 ];
